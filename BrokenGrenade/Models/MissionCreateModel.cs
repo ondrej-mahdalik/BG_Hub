87406813baecbe06edb4a3b121aaa426f1fb7255
Ondřej Mahdalík <ondrej.mahdalik@gmail.com>
2022-10-23 11:14:39 +0200

    Bug fixes

---
+++ BrokenGrenade/Models/MissionCreateModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BrokenGrenade.Models;
+
+public class MissionCreateModel
+{
+    [Required]
+    public string? Name { get; set; }
+    [Required]
+    [Url]
+    public string? SlottingSheetUrl { get; set; }
+    [Required]
+    public DateTime? Date { get; set; }
+    public bool IsLeaderBriefing { get; set; }
+    public DateTime? LeaderBriefing { get; set; }
+    [Required]
+    public DateTime? ConectingToServer { get; set; }
+    [Required]
+    public DateTime? Start { get; set; }
+    public string? ModpackUrl { get; set; }
+    [Required]
+    public Guid? CategoryId { get; set; }
+    public Guid? ModpackTypeId { get; set; }
+}
