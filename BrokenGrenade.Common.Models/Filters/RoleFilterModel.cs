c455020c1644c5a6ea3fc87ee3d675512080a7c5
Milan Šabata <xsabat07@vutbr.cz>
2023-01-12 15:48:25 +0100

    Base for Role part

---
+++ BrokenGrenade.Common.Models/Filters/RoleFilterModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BrokenGrenade.Common.Models.Filters;
+
+public class RoleFilterModel
+{
+    public string? RoleName { get; set; }
+    public bool IsActive { get; set; }
+    public string? UserNickname { get; set; }
+    public string? IssuedByNickname { get; set; }
+}
