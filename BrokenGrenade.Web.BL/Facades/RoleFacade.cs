294432bfb1ad81d2112bba14bef65bac98204bbb
Milan Šabata <xsabat07@vutbr.cz>
2023-01-12 16:54:08 +0100

    Revert "Revert "Added user count to role model""
    This reverts commit 684789ca3220af0830f695526f54cccd86884416.

--- BrokenGrenade.Web.BL/Facades/RoleFacade.cs
+++ BrokenGrenade.Web.BL/Facades/RoleFacade.cs
@@ -13,13 +13,15 @@ public class RoleFacade : IAppFacade
 {
     private readonly IMapper _mapper;
     private readonly RoleManager<RoleEntity> _roleManager;
-    
-    public RoleFacade(IMapper mapper, RoleManager<RoleEntity> roleManager)
+    private readonly UserManager<UserEntity> _userManager;
+
+    public RoleFacade(IMapper mapper, RoleManager<RoleEntity> roleManager, UserManager<UserEntity> userManager)
     {
         _mapper = mapper;
         _roleManager = roleManager;
+        _userManager = userManager;
     }
-    
+
     public async Task DeleteAsync(Guid id)
     {
         var entity = await _roleManager.FindByIdAsync(id.ToString());
@@ -82,6 +84,11 @@ public class RoleFacade : IAppFacade
         var model = _mapper.Map<RoleModel>(entity);
         var claims = await _roleManager.GetClaimsAsync(entity);
         var permissions = claims.Where(x => x.Type == "permission").Select(x => x.Value).ToList();
+        if (entity.Name != null)
+        {
+            var users = await _userManager.GetUsersInRoleAsync(entity.Name);
+            model.UserCount = users.Count;
+        }
 
         model.CreateMissions = permissions.Contains(PermissionTypes.CreateMissions);
         model.ManageMissions = permissions.Contains(PermissionTypes.ManageMissions);
@@ -106,7 +113,6 @@ public class RoleFacade : IAppFacade
             await _roleManager.RemoveClaimAsync(entity, claim);
         
         // Add new permissions
-        var permissions = new List<string>();
         if (model.CreateMissions)
             await _roleManager.AddClaimAsync(entity, new Claim("permission", PermissionTypes.CreateMissions));
         
