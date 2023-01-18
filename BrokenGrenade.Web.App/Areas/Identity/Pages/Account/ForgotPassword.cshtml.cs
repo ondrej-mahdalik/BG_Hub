449b387b937c046f6760581d06df7d5697c78f98
Ondřej Mahdalík <ondrej.mahdalik@gmail.com>
2023-01-18 10:46:09 +0100

    Fixed forgot password email

--- BrokenGrenade.Web.App/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
+++ BrokenGrenade.Web.App/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
@@ -2,15 +2,12 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
 using System.Text.Encodings.Web;
-using System.Threading.Tasks;
 using BrokenGrenade.Web.App.Resources;
 using BrokenGrenade.Web.App.Resources.Areas.Identity.Pages.Account;
 using BrokenGrenade.Web.DAL.Entities;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
