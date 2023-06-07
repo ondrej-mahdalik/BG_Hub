0380a01b23ae1bbef11a9d48f5ace30641e32eb4
Ondřej Mahdalík <ondrej.mahdalik@gmail.com>
2023-06-07 13:00:39 +0200

    Added sidebar collapsing on link click on phone

---
+++ BrokenGrenade.Web.App/wwwroot/js/scripts.js
@@ -0,0 +1,17 @@
+﻿function InitTooltips() {
+    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+}
+
+function EmulateClick(elementId) {
+    const element = document.getElementById(elementId);
+    element.click();
+}
+
+function CollapseSidebar(sidebarId, buttonId) {
+    const element = document.getElementById(sidebarId);
+
+    if (element.classList.contains("show")) {
+        EmulateClick(buttonId);
+    }
+}
--- BrokenGrenade.Web.App/wwwroot/js/tooltip.js
+++
@@ -1,4 +0,0 @@
-﻿function InitTooltips() {
-    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
-    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
-}
